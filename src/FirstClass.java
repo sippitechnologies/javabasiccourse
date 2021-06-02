d7baba44ad2aab29f259c24e524b4eb76f2506a2
sippitechnologies@gmail.com <sippitechnologies@gmail.com>
2021-06-02 08:55:18 +0530

    First Class Code

---
+++ src/FirstClass.java
@@ -0,0 +1,21 @@
+public class FirstClass {
+
+
+
+    public static void main(String[] args) {
+
+        int age;
+        float test;
+        double testing2;
+        boolean isAdult;
+
+        age=30;
+        test=45.5f;
+        testing2=752.9023423;
+        isAdult=false;
+        System.out.println(age);
+        System.out.println(test);
+        System.out.println(isAdult);
+        System.out.println(testing2);
+    }
+}
---
+++ src/Pranab.java
@@ -0,0 +1,57 @@
+public class Pranab {
+
+    public static void main(String[] args) {
+        /*
+                Name can't be start with numeric value
+                Name can't contains special symbol (Expect _ and $)
+                 You can't take a space between name
+                You can't take reserved keyword as variable name
+
+                Class Name class start with Capital Letter  Pranab, PranabKarmanHello
+                Method name start from lowercase letter and another words start from Capital Letter Camel Case
+                Ex: addTwoNumber(), add(),getGreaterNumber(), setGreaterNumber()
+                Maximum  length name in java is 30 character its Recommended
+                Your name should be meaning
+
+                static const variable name WRITE in ALL CapitalLetter
+                instance variable we will follow method naming Convention rules
+         */
+        /*
+               variable Properties
+               1. Data type (See this)
+               2. Name (See this)
+               3. value (see this)
+               4. Memory location (used internally)
+
+         */
+
+        /*
+            Primitive and
+            byte  1
+            short 3
+            char 'a'
+            int  134334
+            long 134334134334
+            float 4.9f;
+            double 90.0000;
+            boolean true and false
+            Non Primitive
+            Class
+            Object
+            Interface
+            String
+            Array
+
+
+
+
+         */
+
+        int ab=10;
+        byte testbyte=9;
+        System.out.println(ab);
+        System.out.println(testbyte);
+
+
+    }
+}
